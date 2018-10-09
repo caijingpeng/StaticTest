e72380ad6251ddcd7557e052f6147fdae9664d14
caijingpeng <caijingpeng0418@gmail.com>
2018-10-09 17:56:57 +0800

    a

---
+++ include/staticTest/staticTest.h
@@ -0,0 +1,13 @@
+//
+//  staticTest.h
+//  staticTest
+//
+//  Created by caijingpeng on 2018/10/9.
+//  Copyright © 2018 caijingpeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface staticTest : NSObject
+
+@end
